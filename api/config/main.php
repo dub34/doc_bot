67a0fe62a2e4b8112ef30e30a801c94a479d0746
victor <dub34@yandex.ru>
2016-10-24 21:18:20 +0300

    enable debg

--- api/config/main.php
+++ api/config/main.php
@@ -108,11 +108,11 @@ $config = [
 
 if (YII_ENV_DEV) {
     // configuration adjustments for 'dev' environment
-    $config['bootstrap'][] = 'debug';
-    $config['modules']['debug'] = [
-        'class' => 'yii\debug\Module',
-        'allowedIPs' => ['*']
-    ];
+//    $config['bootstrap'][] = 'debug';
+//    $config['modules']['debug'] = [
+//        'class' => 'yii\debug\Module',
+//        'allowedIPs' => ['*']
+//    ];
 
 //    $config['bootstrap'][] = 'gii';
 //    $config['modules']['gii'] = [
